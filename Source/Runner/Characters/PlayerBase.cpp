2a4598f8fdb8f44ed5846c0651ff85111fdd3f47
Kamil Waligóra <k.waligora37@gmail.com>
2021-11-10 11:25:00 +0100

    Add PlayerBase class with BP

---
+++ Source/Runner/Characters/PlayerBase.cpp
@@ -0,0 +1,40 @@
+#include "PlayerBase.h"
+
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+APlayerBase::APlayerBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Sets components
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Component"));
+	RootComponent = CapsuleComponent;
+	
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Skeletal Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void APlayerBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
